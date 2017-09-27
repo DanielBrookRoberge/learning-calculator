9a4f4921d9971c2c44931c56a03e3bc383e96daa
Daniel Brook-Roberge <daniel@vclabs.ca>
2017-09-26 17:12:47 -0700

    Start to rewrite using folds

--- haskell/Tokenize.hs
+++ haskell/Tokenize.hs
@@ -13,19 +13,15 @@ pushIntIfNecessary :: (Tokens, String) -> Tokens
 pushIntIfNecessary (tokens, "") = tokens
 pushIntIfNecessary (tokens, int) = tokens ++ [int]
 
-traverseExpr :: (String, Tokens, String) -> (String, Tokens, String)
-traverseExpr ("", x, y) = ("", x, y)
-traverseExpr ((c:expr), tokens, int)
-  | isSeparator c = traverseExpr (expr, tokens, int)
-  | isDigit c = traverseExpr (expr, tokens, int ++ [c])
-  | c `elem` operators = traverseExpr (expr, (pushIntIfNecessary (tokens, int)) ++ [[c]], "")
+traverseExpr :: (Tokens, String) -> Char -> (Tokens, String)
+traverseExpr (tokens, int) c
+  | isSeparator c = (tokens, int)
+  | isDigit c = (tokens, int ++ [c])
+  | c `elem` operators = ((pushIntIfNecessary (tokens, int)) ++ [[c]], "")
   | otherwise = error "Unrecognized character"
 
-discardFirst :: (a, b, c) -> (b, c)
-discardFirst (_, x, y) = (x, y)
-
 parse :: String -> (Tokens, String)
-parse expr = discardFirst $ traverseExpr (expr, [], "")
+parse expr = foldl traverseExpr ([], "") expr
 
 tokenizeExpr :: String -> Tokens
 tokenizeExpr = pushIntIfNecessary . parse
