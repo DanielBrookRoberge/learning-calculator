1172cda5baf1c4bc3089986d626700ea7b140645
Daniel Brook-Roberge <d.brook.roberge@gmail.com>
2017-10-01 21:34:22 -0700

    Add some missing stuff

--- haskell/RPN.hs
+++ haskell/RPN.hs
@@ -4,25 +4,32 @@ where
 
 import Data.Char (isDigit)
 
+type Stack = [Float]
+type Tokens = [String]
+type Binary = (Float -> Float -> Float)
+
 isNumber :: String -> Bool
 isNumber = all isDigit
 
-type Stack = [Float]
-type Tokens = [String]
+operators :: String
+operators = "+-*/"
 
-performOperation :: (Float -> Float -> Float) -> Stack -> Stack
-performOperation _ [] = error "Stack underflow"
-performOperation _ (_:[]) = error "Stack underflow"
-performOperation op (x:y:stack) = (y `op` x):stack
+opFor :: String -> Binary
+opFor "+" = (+)
+opFor "-" = (-)
+opFor "*" = (*)
+opFor "/" = (/)
+opFor _ = error "Unrecognized token"
+
+performOperation :: Stack -> Binary -> Stack
+performOperation [] _ = error "Stack underflow"
+performOperation (_:[]) _ = error "Stack underflow"
+performOperation (x:y:stack) op = (y `op` x):stack
 
 consumeRpnToken :: Stack -> String -> Stack
 consumeRpnToken stack token
   | isNumber token = (read token):stack
-  | token == "+" = performOperation (+) stack
-  | token == "-" = performOperation (-) stack
-  | token == "*" = performOperation (*) stack
-  | token == "/" = performOperation (/) stack
-  | otherwise = error "Unrecognized token"
+  | otherwise = performOperation stack $ opFor token
 
 extractResult :: Stack -> Float
 extractResult [] = error "Stack underflow"
